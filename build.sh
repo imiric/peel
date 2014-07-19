9ee626171d025588d78ca5e130bbd97ab4dc0215
Ivan Miric <imiric@gmail.com>
2014-07-20 01:56:52 +0200

    Refactor build.sh to automatically link project dependencies
    Python dependencies should now only be added in requirements.txt.

--- build.sh
+++ build.sh
@@ -1,22 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
+CWD=$(pwd)
 DOWNLOAD_DIR=./src
+
+# Clean any previous leftovers
+rm -rf "${DOWNLOAD_DIR}"
+mkdir "${DOWNLOAD_DIR}"
 
 # test for git and hg commands
 for cmd in git hg; do
     command -v ${cmd} >/dev/null || { echo "sh: command not found: ${cmd}"; exit 1; }
 done
 
-# download all packages to ./src (default)
-pip install --no-install -r requirements.txt
+# Download all packages to ./src (default)
+pip install --download "${DOWNLOAD_DIR}" --no-use-wheel -r requirements.txt
 
-cp -r ${DOWNLOAD_DIR}/django-autoload/autoload ./autoload
-cp -r ${DOWNLOAD_DIR}/django-dbindexer/dbindexer ./dbindexer
-cp -r ${DOWNLOAD_DIR}/django-nonrel/django ./django
-cp -r ${DOWNLOAD_DIR}/djangoappengine/djangoappengine ./djangoappengine
-cp -r ${DOWNLOAD_DIR}/djangotoolbox/djangotoolbox ./djangotoolbox
+cd "${DOWNLOAD_DIR}"
+# Unpack packages
+ls *.tar.gz | xargs -I{} tar xzf {}
 
-[ -f ${DOWNLOAD_DIR}/pip-delete-this-directory.txt ] && rm -rf ${DOWNLOAD_DIR}
+# Move all dependencies to the project root, so they're available to GAE.
+for dir in $(find . -maxdepth 1 -mindepth 1 -type d); do
+    toplevel=$(find ${dir} -name top_level.txt -type f -exec cat {} \;)
+    if [ -n "${toplevel}" ]; then
+        dep="${dir}/${toplevel}"
+        if [ -d "${dep}" ]; then
+            # There's a directory with the top_level name.
+            mv "${dep}" "${CWD}"
+        elif [ -f "${dep}.py" ]; then
+            # There's a Python module with the top_level name.
+            mv "${dep}.py" "${CWD}"
+        fi
+    else
+        # Parse setup.py to get the module name(s)
+        modules=$(grep py_modules ${dir}/setup.py 2>/dev/null | sed -e 's:.*\[\(.*\)\].*:\1:g' -e 's:[",]: :g')
+        for mod in "${modules}"; do
+            mod=$(echo "${mod}" | tr -d ' ')
+            file="${dir}/${mod}.py"
+            if [ -f "${file}" ]; then
+                mv "${file}" ${CWD}
+            fi
+        done
+    fi
+done
+
+cd "${CWD}"
+rm -rf "${DOWNLOAD_DIR}"
 
 echo "Now run:
 ./manage.py runserver
