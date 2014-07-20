ffe0d2220c5ab22eadb0da1a695ba518f9ea0949
Ivan Miric <imiric@gmail.com>
2014-07-20 14:22:17 +0200

    Add initial Article model

--- peel/models.py
+++ peel/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from djangotoolbox.fields import ListField
 
-# Create your models here.
+class Article(models.Model):
+
+    EDITING   = 1
+    PUBLISHED = 2
+    DELETED   = 3
+    STATUS_CHOICES = (
+        (EDITING, 'Editing'),
+        (PUBLISHED, 'Published'),
+        (DELETED, 'Deleted'),
+    )
+
+    author = models.ForeignKey(User, null=True, blank=True)
+    title = models.CharField(max_length=512)
+    content = models.TextField()
+    tags = ListField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=EDITING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
