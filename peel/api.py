b32a2598cb9aa45a26653983e9eb55bb2858a911
Ivan Miric <imiric@gmail.com>
2014-07-20 18:10:25 +0200

    Fetch articles from server using Ajax

--- peel/api.py
+++ peel/api.py
@@ -4,5 +4,9 @@ from peel.models import Article
 
 
 class ArticleResource(ModelResource):
+    def dehydrate_tags(self, bundle):
+        # Needed to properly serialize tags into a valid JSON list of strings.
+        return bundle.obj.tags
+
     class Meta:
         queryset = Article.objects.all()
--- peel/frontend/scripts/app.js
+++ peel/frontend/scripts/app.js
@@ -10,7 +10,7 @@ var Article = React.createClass({displayName: 'Article',
       React.DOM.div( {className:"article"}, 
         React.DOM.h3( {className:"title"}, this.props.title),
         React.DOM.div( {className:"tags"}, tags),
-        React.DOM.div( {className:"date"}, this.props.created_at.toUTCString()),
+        React.DOM.div( {className:"date"}, this.props.created_at),
         React.DOM.p( {className:"article-body"}, this.props.content)
       )
     );
@@ -18,9 +18,21 @@ var Article = React.createClass({displayName: 'Article',
 });
 
 var ArticleList = React.createClass({displayName: 'ArticleList',
+  getInitialState: function() {
+    return {articles: []};
+  },
+
+  componentDidMount: function() {
+    $.get(this.props.source, function(result) {
+      if (this.isMounted()) {
+        this.setState({articles: result.objects});
+      }
+    }.bind(this));
+  },
+
   render: function() {
     var articles = [];
-    this.props.articles.forEach(function(article) {
+    this.state.articles.forEach(function(article) {
       articles.push(Article( {title:article.title, content:article.content,
                     tags:article.tags, created_at:article.created_at} ));
     });
@@ -32,10 +44,4 @@ var ArticleList = React.createClass({displayName: 'ArticleList',
   }
 });
 
-var ARTICLES = [
-  {title: 'My first post', content: 'Well this is cool.', tags: ['dumb', 'funny'], created_at: new Date('2014-07-01T08:24:39.184')},
-  {title: 'Another post', content: 'Radical!', tags: ['sweet'], created_at: new Date('2014-07-13T18:21:13.591')},
-  {title: 'And yet another', content: 'Cowabunga!', tags: [], created_at: new Date('2014-06-26T13:51:50.417')},
-];
-
-React.renderComponent(ArticleList( {articles:ARTICLES} ), document.getElementById("content"));
+React.renderComponent(ArticleList( {source:"/api/v1/article/"} ), document.getElementById("content"));
