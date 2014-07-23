ffb267152775b344d4acb16e71603a04c342321c
Ivan Miric <imiric@gmail.com>
2014-07-23 17:03:07 +0200

    Add button to add a new article, setup Bootstrap tooltip

--- peel/frontend/scripts/app.js
+++ peel/frontend/scripts/app.js
@@ -1,3 +1,4 @@
 'use strict';
 
 require('./react.js');
+require('./main.js');
---
+++ peel/frontend/scripts/main.js
@@ -0,0 +1,5 @@
+var domready = require('domready/ready');
+
+domready(function() {
+  $('button.add-article').tooltip();
+});
